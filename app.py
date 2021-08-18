683907bf64841fe6df3e0008984632652d155167
Md. Abdullah Ibna Harun <abdullah13426.cse@gmail.com>
2021-08-17 22:08:46 -0700

    test successful

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB 
+
 
 app = Flask(__name__)
 
